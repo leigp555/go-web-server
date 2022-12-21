18e8804630ffd9c75771dcc396d246d5b11869a0
leigp <907090585@qq.com>
2022-12-21 17:51:31 +0800

    feat: 连接配置mysql

---
+++ util/mysql_link.go
@@ -0,0 +1,37 @@
+package util
+
+import (
+	"fmt"
+	"go/note/config"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+)
+
+type myDb struct {
+	db *gorm.DB
+}
+
+var Mydb = new(myDb)
+
+// LinkMysqlDB LinkDB 连接mysql数据库
+func (db *myDb) LinkMysqlDB() *gorm.DB {
+	dsn := fmt.Sprintf("root:%s@tcp(%s)/note?charset=utf8mb4&parseTime=True", config.GlobalConfig.Mysql.Password, config.GlobalConfig.Mysql.Addr)
+	d, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info), //配置一个日志
+	})
+	if err != nil {
+		log.Panicf("mysql数据库连接失败%v\n", err)
+		return nil
+	}
+	sqlDb, _ := d.DB()
+	defer sqlDb.Close()
+
+	//设置连接池
+	sqlDb.SetMaxIdleConns(config.GlobalConfig.Mysql.MaxConn) //设置最大连接数
+	sqlDb.SetMaxOpenConns(config.GlobalConfig.Mysql.MaxConn) //设置最大的空闲连接数
+	Mydb.db = d
+	fmt.Println("成功连接mysql数据库")
+	return d
+}
